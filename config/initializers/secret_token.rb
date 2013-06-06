dab2041ee2e18916a8f039a8218ab787ace73d1b
Jonathan Martin <me@nybblr.com>
2013-06-06 17:28:39 -0400

    Initial: rails new awesome -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awesome::Application.config.secret_key_base = '198344927df6f966b4d315fb0344fc54822cb7a20e8402ef395725ea73da9385b656590fce3bed19002d497af16450679484171482026e058550b208987f0612'
